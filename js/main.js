49b46e56f5d20205f17ee9453a533fe458497dc2
cinqueterreli <Anthony@Anthonys-MBP.home>
2015-07-20 15:40:46 -0400

    weee

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
 
+
+$(document).ready(function(){
+
+
+/*--- Dropdown Menu Functionality ---*/
+
+
+/* -------- Hamburger ----------- */
+
+  $('.hamburger').click(function(){
+    $('.mobile-nav').slideDown(500);
+    $(this).css("display", "none")
+    $('.x').css("display", 'inline-block')
+  });
+
+    $('.x').click(function(){
+    $('.mobile-nav').slideUp(500);
+    $(this).css("display", "none")
+    $('.hamburger').css("display", 'inline-block')
+  });
+
+
+
+// ------ Media Queries ------ //
+
+  enquire.register("screen and (min-width:1100px)",{
+    match : function() {
+      $('.mobile-nav').css('display', 'none')
+      $('.x').css('display','none')
+      $('.hamburger').css('display', 'none')
+    },
+    unmatch : function() {
+      $('.hamburger').css('display', 'inline-block')
+    } 
+
+  });
+
+
+});
